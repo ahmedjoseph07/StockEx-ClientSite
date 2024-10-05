7a88189af2d514112d8adcfc97d48433cd88e581
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2024-10-05 19:50:50 +0600

    support-page design

--- src/landing_page/support/Hero.jsx
+++ src/landing_page/support/Hero.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <div>
-      hero
+      <section className="mx-auto text-center">
+        <div>
+          <h1 className="font-bold text-4xl my-2 py-1">Support Portal</h1>
+          <p className="my-6"><a className="hover:underline hover:text-black bg-green-600 rounded-lg p-2 font-bold" href="">Track Tickets</a></p>
+        </div>
+        <div className="mx-4 p-1">
+          <form className="max-w-md mx-auto">
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search your queries"
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute end-2.5 bottom-2.5 bg-green-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="flex flex-col mx-2 my-1 p-1 gap-y-1 font-light text-black">
+          <h1 className="font-bold">Additional Links</h1>
+          <a className="hover:underline hover:text-green-600" href="">Track account opening</a>
+          <a className="hover:underline hover:text-green-600" href="">Track segment activation</a>
+          <a className="hover:underline hover:text-green-600" href="">Intraday margins</a>
+          <a className="hover:underline hover:text-green-600" href="">Kite user manual</a>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/landing_page/support/RaiseTicket.jsx
+++ src/landing_page/support/RaiseTicket.jsx
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from "react";
 
 const RaiseTicket = () => {
   return (
     <div>
-      raise
-    </div>
-  )
-}
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <div className="max-w-screen-md mb-8 lg:mb-16">
+            <h2 className="mb-2 text-4xl tracking-tight font-semibold text-gray-900 dark:text-white">
+              To create a ticket, select a relevant topic
+            </h2>
+          </div>
+          <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <i className="fa-solid fa-plus"></i>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Account Opening
+              </h3>
+              <div className="flex flex-col mx-2 my-1 p-1 gap-y-1 font-light text-black">
+              <a className="hover:underline hover:text-green-600" href="">Getting started</a>
+              <a className="hover:underline hover:text-green-600" href="">Online</a>
+              <a className="hover:underline hover:text-green-600" href="">Offline</a>
+              <a className="hover:underline hover:text-green-600" href="">Charges</a>
+              <a className="hover:underline hover:text-green-600" href="">Company, Partnership and HUF</a>
+              </div>
+            </div>
 
-export default RaiseTicket
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <i className="fa-solid fa-user"></i>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Your StockEx Account</h3>
+              <div className="flex flex-col mx-2 my-1 p-1 gap-y-1 font-light text-black">
+              <a className="hover:underline hover:text-green-600" href="">Login credentials</a>
+              <a className="hover:underline hover:text-green-600" href="">Your Profile</a>
+              <a className="hover:underline hover:text-green-600" href="">Account modification and segment addition</a>
+              <a className="hover:underline hover:text-green-600" href="">CMR & DP ID</a>
+              <a className="hover:underline hover:text-green-600" href="">Transfer and conversion of shares</a>
+              </div>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <i className="fa-duotone fa-solid fa-bars"></i>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Trading and Markets
+              </h3>
+              <div className="flex flex-col mx-2 my-1 p-1 gap-y-1 font-light text-black">
+              <a className="hover:underline hover:text-green-600" href="">Trading FAQs</a>
+              <a className="hover:underline hover:text-green-600" href="">Kite</a>
+              <a className="hover:underline hover:text-green-600" href="">Margins</a>
+              <a className="hover:underline hover:text-green-600" href="">Product and order types</a>
+              <a className="hover:underline hover:text-green-600" href="">Corporate actions</a> 
+              <a className="hover:underline hover:text-green-600" href="">Kite Features</a> 
+              </div>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <i className="fa-solid fa-briefcase"></i>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Funds
+              </h3>
+              <div className="flex flex-col mx-2 my-1 p-1 gap-y-1 font-light text-black">
+              <a className="hover:underline hover:text-green-600" href="">Fund withdrawal</a>
+              <a className="hover:underline hover:text-green-600" href="">Adding funds</a>
+              <a className="hover:underline hover:text-green-600" href="">Adding bank accounts</a>
+              <a className="hover:underline hover:text-green-600" href="">eMandates</a>
+              </div>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <i className="fa-regular fa-circle"></i>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+                Enterprise Design
+              </h3>
+              <div className="flex flex-col mx-2 my-1 p-1 gap-y-1 font-light text-black">
+              <a className="hover:underline hover:text-green-600" href="">IPO</a>
+              <a className="hover:underline hover:text-green-600" href="">Portfolio</a>
+              <a className="hover:underline hover:text-green-600" href="">Funds statement</a>
+              <a className="hover:underline hover:text-green-600" href="">Profile</a>
+              <a className="hover:underline hover:text-green-600" href="">Reports</a>
+              <a className="hover:underline hover:text-green-600" href="">Referral program</a>
+              </div>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                <i className="fa-solid fa-circle"></i>
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">
+                Coin
+              </h3>
+              <div className="flex flex-col mx-2 my-1 p-1 gap-y-1 font-light text-black">
+              <a className="hover:underline hover:text-green-600" href="">Understanding mutual funds and Coin</a>
+              <a className="hover:underline hover:text-green-600" href="">Coin app</a>
+              <a className="hover:underline hover:text-green-600" href="">Coin web</a>
+              <a className="hover:underline hover:text-green-600" href="">Transactions and reports</a>
+              <a className="hover:underline hover:text-green-600" href="">National Pension Scheme (NPS)</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default RaiseTicket;
